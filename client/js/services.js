f7e84490b8928fd66fde3c8659b13b603cf7b0d3
priyanshujindal1995@gmail.com <priyanshujindal1995@gmail.com>
2015-07-20 03:21:41 +0530

    services.js added

---
+++ client/js/services.js
@@ -0,0 +1,23 @@
+portfolioApp.factory('menuHandler', function() {
+
+  var showAll = function() {
+    var allLists = document.getElementsByClassName('menu-list')
+    for (var i = 0; i < allLists.length; i++) {
+      angular.element(allLists[i]).removeClass('active')
+    }
+  }
+
+  var hide = function(element) {
+    angular.element(document.getElementsByClassName(element)).addClass('active')
+  }
+
+  var onPage = function(pageName) {
+    showAll()
+    hide(pageName)
+  }
+
+  return {
+    value:true,
+    onPage: onPage
+  }
+})
