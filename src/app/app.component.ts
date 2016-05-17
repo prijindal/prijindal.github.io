d047784c184b6658bb38b313850e9f75801bfa2c
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-05-17 18:33:18 +0530

    Moved Message box to its own component and added its overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core'
 
+import { MessageBox } from './message-box/message-box'
+
 @Component({
   selector:'my-app',
   template:require('./app.component.html'),
-  styles:[require('./app.component.scss')]
+  styles:[require('./app.component.scss')],
+  directives:[MessageBox]
 })
 export class App {
   text = 'My App'
---
+++ src/app/message-box/message-box.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector:'message-box',
+  template:require('./message-box.html'),
+  styles:[require('./message-box.scss')]
+})
+export class MessageBox {
+  
+}
