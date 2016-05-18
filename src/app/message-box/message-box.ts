66560e8eb6f297d93e2e8898000cff841809e046
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-05-18 10:45:06 +0530

    Submitted Form Message completed

--- src/app/message-box/message-box.ts
+++ src/app/message-box/message-box.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core'
   styles:[require('./message-box.scss')]
 })
 export class MessageBox {
+  messaging:boolean = false
+  form = {message:''}
+  textarearows = 3
+  submitted:boolean = false
 
+  calculateRows() {
+    var length = this.form.message.split("\n").length + 1
+    this.textarearows = length > 3 ? length : 3
+  }
+
+  sendMessage(e) {
+    e.preventDefault()
+    console.log(this.form)
+    this.submitted = true
+  }
+
+  closeThis() {
+    this.messaging = false
+    this.submitted = false
+    this.form = {message:''}
+    this.calculateRows()
+  }
+
+  toggleThis() {
+    if(this.messaging) return this.closeThis()
+    this.messaging = true
+  }
 }
