12330ad12b83acf72a034f41c4a16340243e9ff0
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-10-21 09:59:44 +0530

    Added User and email for automatic deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@ ng build -prod --aot &&
 cd dist && 
 git init && 
 git add . && 
-git commit -am 'Init commit' && 
-git remote add origin git@github.com:prijindal/prijindal.github.io.git
+git commit -am 'Init commit' &&
+git config user.email $GH_EMAIL &&
+git config user.name $GH_NAME && 
+git remote add origin $GH_REF &&
 git push origin master --force
