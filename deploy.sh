10d170ff7011c2d597bc82513c24b49fba0db0ed
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-10-21 10:26:59 +0530

    Fixed Gh ref

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ git add . &&
 git config user.email $GH_EMAIL &&
 git config user.name $GH_NAME && 
 git commit -am 'Init commit' &&
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
