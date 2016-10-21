1e6d6eaa6cbe53be7967213d1899505f6fce0c18
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2016-10-21 10:03:32 +0530

    Fixed user and email

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 ng build -prod --aot && 
 cd dist && 
+git config user.email $GH_EMAIL &&
+git config user.name $GH_NAME && 
 git init && 
 git add . && 
 git commit -am 'Init commit' &&
-git config user.email $GH_EMAIL &&
-git config user.name $GH_NAME && 
 git remote add origin $GH_REF &&
 git push origin master --force
