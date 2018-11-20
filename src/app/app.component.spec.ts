0e793760790358e233cbb69664d758c4ac8616ee
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-11-20 15:15:42 +0530

    Added chromeheadless tests

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,9 @@
+/* tslint:disable:no-unused-variable */
+
+import { async } from '@angular/core/testing';
+
+describe('App: PriyanshuJindal', () => {
+  it('should check dummy test', async(() => {
+    expect(2).toBeLessThanOrEqual(2);
+  }));
+});
