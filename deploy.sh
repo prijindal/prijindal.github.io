5b4871c299faf95669a7bde6f7fc32310ff12c62
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-11-20 15:09:36 +0530

    Fixed tests

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-ng build --prod --aot && 
-cd dist && 
-git init && 
-git add . && 
-git config user.email $GH_EMAIL &&
-git config user.name $GH_NAME && 
-git commit -am 'Init commit' &&
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
