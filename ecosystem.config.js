e41d528f32d983170b6233c4370484c6fb036507
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-11-20 17:53:04 +0530

    Added ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  apps: [
+    {
+      name: 'SERVER',
+      script: 'node',
+
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      args: 'dist/server.js',
+      instances: 1,
+      env: {
+        watch: true,
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        watch: false,
+        NODE_ENV: 'production',
+        autorestart: true,
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: '~/Downloads/newsapp.pem',
+      user: 'ubuntu',
+      host: '13.126.205.26',
+      ref: 'origin/master',
+      repo: 'ssh://git@gitlab.com/prijindal/prijindal.github.io.git',
+      path: '/home/ubuntu/prijindal',
+      'post-deploy':
+        'yarn install --production && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
