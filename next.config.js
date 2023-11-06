d39c93fb23dbdcdc00dd5ee13d0442166a5de73c
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2023-11-06 12:45:20 +0530

    fixed exporting html output

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const NEXT_OUTPUT_TYPE = process.env.NEXT_OUTPUT_TYPE;
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: NEXT_OUTPUT_TYPE
+}
 
 module.exports = nextConfig
