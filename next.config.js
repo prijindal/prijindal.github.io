0161f2deaaf0201add3e9db8387a075f91d0cc01
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2023-11-06 13:00:35 +0530

    Added prettier

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const NEXT_OUTPUT_TYPE = process.env.NEXT_OUTPUT_TYPE;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: NEXT_OUTPUT_TYPE
-}
+  output: NEXT_OUTPUT_TYPE,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
